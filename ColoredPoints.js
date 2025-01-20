1359ae4e9ab7bfc5ec53ca16042cac168e68d0a7
Size Liu <sliu236@ucsc.edu>
2025-01-20 04:34:16 -0800

    final version

--- ColoredPoints.js
+++ ColoredPoints.js
@@ -22,6 +22,7 @@ let gl;
 let a_Position;
 let u_FragColor;
 let u_Size;
+let isRandomColorMode = false;
 
 function setupWebGL() {
     // Retrieve <canvas> element
@@ -72,6 +73,7 @@ const CIRCLE = 2;
 let g_selectedColor = [1.0, 1.0, 1.0, 1.0]; // White
 let g_selectedSize = 5; // Default point size
 let g_selectedType = POINT;
+let g_selectedSegments = 10;
 
 function addActionForHtmlUI() {
 
@@ -79,7 +81,9 @@ function addActionForHtmlUI() {
   document.getElementById('red').onclick = function() { g_selectedColor = [1.0,0.0,0.0,1.0]; }; // Red
   document.getElementById('blue').onclick = function() { g_selectedColor = [0.0,0.0,1.0,1.0]; }; // Blue
   document.getElementById('clearButton').onclick = function() { g_shapeList = []; renderAllShapes()}; // Clear
-
+  document.getElementById('drawPicture').onclick = function() {drawPicture();}; // Draw Picture
+  document.getElementById('enableRandomColor').onclick = function() { isRandomColorMode = true; console.log('Randon mode enabled, have fun!');}; 
+  document.getElementById('disableRandomColor').onclick = function() { isRandomColorMode = false; reseteverything(); console.log('Randon mode disabled, you can select color now!');};
 
   document.getElementById('pointButton').onclick = function() { g_selectedType = POINT}; // Point
   document.getElementById('triButton').onclick = function() { g_selectedType = TRIANGLE}; // Triangle
@@ -91,6 +95,11 @@ function addActionForHtmlUI() {
 
   // Size slider event handling
   document.getElementById('sizeSlide').addEventListener('mouseup', function() { g_selectedSize = this.value; }); // Size
+  document.getElementById('segmentSlide').addEventListener('mouseup', function() {g_selectedSegments = parseInt(this.value); }); // Size
+}
+
+function getRandomColor() {
+  return [Math.random(), Math.random(), Math.random(), 1.0]; // 随机RGBA颜色
 }
 
 function main() {
@@ -126,6 +135,15 @@ function click(ev) {
   let [x, y] = convertCoordinatesEventToGL(ev);
 
   let point;
+  if(isRandomColorMode) {
+    let randomType = Math.floor(Math.random() * 3); // randon type
+    g_selectedType = randomType;
+
+    let randomSize = Math.random() * 0 + 55; // random size
+    g_selectedSize = randomSize;
+
+    g_selectedColor = getRandomColor();
+  }
   if(g_selectedType == POINT) { 
     point = new Point();
   } else if (g_selectedType == TRIANGLE) { 
@@ -134,7 +152,7 @@ function click(ev) {
     point = new Circle();
   }
   point.position = [x, y];
-  point.color = g_selectedColor.slice();
+  point.color = isRandomColorMode ? getRandomColor() : g_selectedColor.slice();
   point.size = g_selectedSize;
   g_shapeList.push(point);
 
@@ -175,5 +193,98 @@ function sentTextToHTML(text, htmlID) {
   }
   htmlElm.innerHTML = text;
 }
+
+function resetCanvas() {
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
+function reseteverything() {
+  g_selectedColor = [1.0, 1.0, 1.0, 1.0];
+  g_selectedSize = 5;
+  g_selectedType = POINT;
+  g_selectedSegments = 10;
+
+  document.getElementById('redSlide').value = g_selectedColor[0] * 100;
+  document.getElementById('greenSlide').value = g_selectedColor[1] * 100;
+  document.getElementById('blueSlide').value = g_selectedColor[2] * 100;
+  document.getElementById('sizeSlide').value = g_selectedSize;
+  document.getElementById('segmentSlide').value = g_selectedSegments;
+  resetCanvas();
+}
+
+
+function drawPicture() {
+  gl.clearColor(0.53, 0.80, 0.98, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  // Left fuseelage（white)
+  gl.uniform4f(u_FragColor, 1.0, 1.0, 1.0, 1.0); 
+  drawTriangle([0.0, 0.5, -0.05, 0.45, 0.0, 0.4]);
+  drawTriangle([-0.05, 0.45, -0.1, 0.4, 0.0, 0.4]);
+  drawTriangle([-0.1, 0.4, 0.0, 0.4, -0.1, 0.3]);
+  drawTriangle([0.0, 0.4, -0.1, 0.3, 0.0, 0.2]);
+  drawTriangle([-0.1, 0.3, 0.0, 0.2, -0.1, 0.1]);
+  drawTriangle([0.0, 0.2, -0.1, 0.1, 0.0, 0.0]);
+  drawTriangle([-0.1, 0.1, 0.0, 0.0, -0.1, -0.1]);
+  drawTriangle([0.0, 0.0, -0.1, -0.1, 0.0, -0.2]);
+  drawTriangle([-0.1, -0.1, 0.0, -0.2, -0.1, -0.3]);
+  drawTriangle([0.0, -0.2, -0.1, -0.3, 0.0, -0.3]);
+  drawTriangle([-0.1, -0.4, 0.0, -0.3, -0.1, -0.3]);
+
+  // right fuseelage（white)
+  drawTriangle([0.0, 0.5, 0.05, 0.45, 0.0, 0.4]);
+  drawTriangle([0.05, 0.45, 0.1, 0.4, 0.0, 0.4]);
+  drawTriangle([0.1, 0.4, 0.0, 0.4, 0.1, 0.3]);
+  drawTriangle([0.0, 0.4, 0.1, 0.3, 0.0, 0.2]);
+  drawTriangle([0.1, 0.3, 0.0, 0.2, 0.1, 0.1]);
+  drawTriangle([0.0, 0.2, 0.1, 0.1, 0.0, 0.0]);
+  drawTriangle([0.1, 0.1, 0.0, 0.0, 0.1, -0.1]);
+  drawTriangle([0.0, 0.0, 0.1, -0.1, 0.0, -0.2]);
+  drawTriangle([0.1, -0.1, 0.0, -0.2, 0.1, -0.3]);
+  drawTriangle([0.0, -0.2, 0.1, -0.3, 0.0, -0.3]);
+  drawTriangle([0.1, -0.4, 0.0, -0.3, 0.1, -0.3]);
+
+  // left engine（green)
+  gl.uniform4f(u_FragColor, 0.0, 1.0, 0.0, 1.0); 
+  drawTriangle([-0.2, 0.3, -0.2, 0.2, -0.1, 0.3]);
+
+  // right engine（green)
+  drawTriangle([0.2, 0.2, 0.2, 0.3, 0.1, 0.3]);
+
+  // left wing（blue）
+  gl.uniform4f(u_FragColor, 0.0, 0.0, 1.0, 1.0); 
+  drawTriangle([-0.1, 0.3, -0.1, 0.0, -0.2, 0.0]);
+  drawTriangle([-0.1, 0.3, -0.2, 0.0, -0.3, 0.2]);
+  drawTriangle([-0.2, 0.0, -0.3, 0.0, -0.3, 0.2]);
+  drawTriangle([-0.3, 0.0, -0.4, 0.0, -0.3, 0.2]);
+  drawTriangle([-0.4, 0.0, -0.3, 0.2, -0.5, 0.1]);
+  drawTriangle([-0.5, 0.1, -0.5, 0.0, -0.4, 0.0]);
+
+  // right wing（blue）
+  drawTriangle([0.1, 0.3, 0.1, 0.0, 0.2, 0.0]);
+  drawTriangle([0.1, 0.3, 0.2, 0.0, 0.3, 0.2]);
+  drawTriangle([0.2, 0.0, 0.3, 0.0, 0.3, 0.2]);
+  drawTriangle([0.3, 0.0, 0.4, 0.0, 0.3, 0.2]);
+  drawTriangle([0.4, 0.0, 0.3, 0.2, 0.5, 0.1]);
+  drawTriangle([0.5, 0.1, 0.5, 0.0, 0.4, 0.0]);
+
+  // left tail wing（yellow and red）
+  gl.uniform4f(u_FragColor, 1.0, 1.0, 0.0, 1.0); 
+  drawTriangle([-0.1, -0.2, -0.3, -0.3, -0.3, -0.4]);
+  gl.uniform4f(u_FragColor, 1.0, 0.0, 0.0, 1.0); 
+  drawTriangle([-0.1, -0.2, -0.1, -0.3, -0.3, -0.35]);
+
+  // right tail wing（yellow and red）
+  gl.uniform4f(u_FragColor, 1.0, 1.0, 0.0, 1.0); 
+  drawTriangle([0.1, -0.2, 0.3, -0.3, 0.3, -0.4]);
+  gl.uniform4f(u_FragColor, 1.0, 0.0, 0.0, 1.0); 
+  drawTriangle([0.1, -0.2, 0.1, -0.3, 0.3, -0.35]);
+
+  document.getElementById('clearButton').onclick = function() { resetCanvas();}; // Clear
+}
+
+
+
 
 
